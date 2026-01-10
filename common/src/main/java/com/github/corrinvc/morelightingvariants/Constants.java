b0a9b9a6e6288345bd6075a0de2a5f674c1f8544
CorrinVC <corrinvc@gmail.com>
2026-01-10 14:23:48 -0500

    Update to 1.21.11

--- common/src/main/java/com/github/corrinvc/morelightingvariants/Constants.java
+++ common/src/main/java/com/github/corrinvc/morelightingvariants/Constants.java
@@ -1,8 +1,8 @@
 package com.github.corrinvc.morelightingvariants;
 
 import net.minecraft.core.Registry;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +16,7 @@ public class Constants {
 		return ResourceKey.create(registry, make(name));
 	}
 
-	public static ResourceLocation make(String name) {
-		return ResourceLocation.fromNamespaceAndPath(MOD_ID, name);
+	public static Identifier make(String name) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, name);
 	}
 }
